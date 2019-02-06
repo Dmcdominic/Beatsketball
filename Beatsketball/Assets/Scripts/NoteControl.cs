b9ec4a4088e3671cc628bc9852b6ec97b0831b2e
Zixuan Zou <>
2019-02-06 14:30:06 -0500

    added notes

---
+++ Beatsketball/Assets/Scripts/NoteControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteControl : MonoBehaviour
+{
+    public float speed;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = speed / 60f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position -= new Vector3(speed * Time.deltaTime, 0f, 0f);
+       
+    }
+}
