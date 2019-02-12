4e9d9d006b377a619eaca2da570ec518dd3183a4
CJ Nilson <cjnilson@andrew.cmu.edu>
2019-02-11 19:59:45 -0500

    Offense and defense control

--- Beatsketball/Assets/Scripts/Defensive Player/defense_script.cs
+++ Beatsketball/Assets/Scripts/Defensive Player/defense_script.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class defense_script : MonoBehaviour
 {
-    public GameObject defense;
+    public GameObject defense1;
+    public GameObject defense2;
+    public GameObject defense3;
+    public GameObject offense;
 
     private int lane = 2;
+    private int counter = 0;
+    private GameObject defense;
 
     private Vector3 moveDirection = Vector3.zero;
     private CharacterController controller;
@@ -15,6 +20,9 @@ public class defense_script : MonoBehaviour
     void Start()
     {
         controller = GetComponent<CharacterController>();
+
+        // establish defensive player
+        defense = defense1;
     }
 
     // Update is called once per frame
@@ -22,29 +30,49 @@ public class defense_script : MonoBehaviour
     {
         float p2_vertical_input = Input.GetAxis("Vertical_2");
 
-        moveDirection = new Vector3(0, 0, 0.25F);
+        moveDirection = new Vector3(0, 0.8333F, 0);
 
+        // counter for lanes
+        counter += 1;
+
+        // switch lanes
         if (music_manager.is_valid_big_frame())
         {
-            if (p2_vertical_input > 0 && lane > 1)
+            if (p2_vertical_input > 0 && lane > 1 && counter >= 30)
             {
                 defense.transform.position += moveDirection;
                 lane -= 1;
+                counter = 0;
             }
 
-            if (p2_vertical_input < 0 && lane < 3)
+            if (p2_vertical_input < 0 && lane < 3 && counter >= 30)
             {
                 defense.transform.position -= moveDirection;
                 lane += 1;
+                counter = 0;
+            }
+        }
+
+        // switch defensive player
+        if (offense.transform.position.x > defense.transform.position.x)
+        {
+            if (defense == defense1)
+            {
+                defense = defense2;
+            } else if (defense == defense2)
+            {
+                defense = defense3;
             }
         }
     }
 
-    private void OnControllerColliderHit(ControllerColliderHit hit)
+    // enter faceoff
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (hit.gameObject.tag == "Offense")
+        if (collision.gameObject.tag == "Offense")
         {
-            //Enter Face Off
+            //enter faceoff
         }
+
     }
 }
--- Beatsketball/Assets/Scripts/Offensive Player/offense_script.cs
+++ Beatsketball/Assets/Scripts/Offensive Player/offense_script.cs
@@ -7,8 +7,8 @@ public class offense_script : MonoBehaviour
     public GameObject offense;
     public float speed = 2.0F;
 
-    private bool hasBall = true;
     private int lane = 2;
+    private int counter = 0;
 
     private Vector3 moveDirection = Vector3.zero;
     private Vector3 forward = Vector3.zero;
@@ -25,33 +25,40 @@ public class offense_script : MonoBehaviour
     {
         float p1_vertical_input = Input.GetAxis("Vertical_1");
 
-        moveDirection = new Vector3(0, 0, 0.25F);
+        moveDirection = new Vector3(0, 2.5F, 0);
         forward = new Vector3(0.025F, 0, 0);
 
         offense.transform.position += forward;
 
+        // counter for lanes
+        counter += 1;
+
+        // switch lanes
         if (music_manager.is_valid_big_frame() == true)
         {
-            if (p1_vertical_input > 0 && lane > 1)
+            if (p1_vertical_input > 0 && lane > 1 && counter >= 30)
             {
                 offense.transform.position += moveDirection;
                 lane -= 1;
+                counter = 0;
             }
 
-            if (p1_vertical_input < 0 && lane < 3)
+            if (p1_vertical_input < 0 && lane < 3 && counter >= 30)
             {
                 offense.transform.position -= moveDirection;
                 lane += 1;
+                counter = 0;
             }
         }
     }
 
-    private void OnControllerColliderHit(ControllerColliderHit hit)
+    // enter faceoff
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (hit.gameObject.tag == "Defense")
+        if (collision.gameObject.tag == "Defense")
         {
-            offense.transform.position = new Vector3(-5, -0.5F, -12.5F);
-            //Enter Face Off
+            offense.transform.position = new Vector3(-6, 0, 49);
         }
+
     }
 }
