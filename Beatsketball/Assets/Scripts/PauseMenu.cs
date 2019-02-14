42b7fd6990d21e3edb2b14f8180b9c2bc37a1004
Dmcdominic <dmcdominic@gmail.com>
2019-02-14 14:38:03 -0500

    Integrated main menu (now with controller support)

--- Beatsketball/Assets/Scripts/PauseMenu.cs
+++ Beatsketball/Assets/Scripts/PauseMenu.cs
@@ -2,45 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PauseMenu : MonoBehaviour
-{
-    public static bool isPaused = false;
-    public GameObject pauseMenuUI;
+public class PauseMenu : MonoBehaviour {
+	public static bool isPaused = false;
+	public GameObject pauseMenuUI;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        pauseMenuUI.SetActive(false);
-    }
+	private float prev_timeScale = 1f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (isPaused)
-            {
-                Resume();
-            }
-            else
-            {
-                Pause();
-            }
-        }
-    }
 
-    void Resume()
-    {
-        pauseMenuUI.SetActive(false);
-        Time.timeScale = 1f;
-        isPaused = false;
+	// Start is called before the first frame update
+	void Start() {
+		pauseMenuUI.SetActive(false);
+	}
 
-    }
+	// Update is called once per frame
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			if (isPaused) {
+				Resume();
+			} else {
+				Pause();
+			}
+		}
+	}
 
-    void Pause()
-    {
-        pauseMenuUI.SetActive(true);
-        Time.timeScale = 0f;
-        isPaused = true;
-    }
+	void Resume() {
+		pauseMenuUI.SetActive(false);
+		Time.timeScale = prev_timeScale;
+		isPaused = false;
+	}
+
+	void Pause() {
+		pauseMenuUI.SetActive(true);
+		prev_timeScale = Time.timeScale;
+		Time.timeScale = 0f;
+		isPaused = true;
+	}
 }
--- Beatsketball/Assets/Scripts/QuitOnClick.cs
+++ Beatsketball/Assets/Scripts/QuitOnClick.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class QuitOnClick : MonoBehaviour
-{
-    public void OnClick()
-    {
-        Application.Quit();
-    }
+public class QuitOnClick : MonoBehaviour {
+	public void OnClick() {
+#if UNITY_EDITOR
+		EditorApplication.isPlaying = false;
+#endif
+#if UNITY_STANDALONE
+		Application.Quit();
+#endif
+	}
 
 }
