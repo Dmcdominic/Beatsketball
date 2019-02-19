fe10778074e89c707a9915328f813977ad489757
Dmcdominic <dmcdominic@gmail.com>
2019-02-19 12:27:53 -0500

    Song Selection screen

--- Beatsketball/Assets/Scripts/Music Scripts/track.cs
+++ Beatsketball/Assets/Scripts/Music Scripts/track.cs
@@ -6,12 +6,17 @@ using UnityEngine;
 public class track : ScriptableObject {
 
 	public AudioClip song;
+	public string title;
+	public string artist;
 	public float bpm;
 	public float start_time;
+
+	public float preview_start_time;
 
 	[Range(0, 1)]
 	public float volume = 0.6f;
 
 	public bool single_airhorn_override = false;
+	public Sprite album_cover;
 
 }
---
+++ Beatsketball/Assets/Scripts/Song Selection/song_select_panel.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class song_select_panel : MonoBehaviour {
+
+	public master_music master_Music;
+	public AudioSource song_preview_player;
+	public song_select_button Song_Select_Button_prefab;
+	
+	public float song_preview_duration;
+	public Color current_selected_color;
+
+	private List<song_select_button> song_Select_Buttons;
+
+
+	// Start is called before the first frame update
+	void Start() {
+		song_Select_Buttons = new List<song_select_button>();
+
+		foreach(track Track in master_Music.all_tracks) {
+			song_select_button new_song_button = Instantiate(Song_Select_Button_prefab, transform);
+			new_song_button.Track = Track;
+			new_song_button.song_Select_Panel = this;
+			song_Select_Buttons.Add(new_song_button);
+
+			if (master_Music.current_track == Track) {
+				Button button = new_song_button.GetComponent<Button>();
+				set_color(button, current_selected_color);
+			}
+		}
+	}
+
+	// Called when a song_select_button is pressed
+	public void on_song_select(song_select_button selected_button) {
+		// Update master_Music
+		master_Music.current_track = selected_button.Track;
+
+		// Play the preview
+		StopAllCoroutines();
+		float volume = master_Music.master_volume * selected_button.Track.volume;
+		StartCoroutine(play_song_preview(selected_button.Track, volume));
+
+		// Update coloring
+		foreach (song_select_button song_button in song_Select_Buttons) {
+			set_color(song_button.button, Color.clear);
+		}
+		set_color(selected_button.button, current_selected_color);
+	}
+
+	// Set the color of a button
+	private void set_color(Button button, Color color) {
+		ColorBlock newColorBlock = button.colors;
+		newColorBlock.normalColor = color;
+		button.colors = newColorBlock;
+	}
+
+	// Play the song preview, managing fade-in and fade-out
+	IEnumerator play_song_preview(track Track, float volume) {
+		float fade_in_duration = 0.5f;
+		float fade_out_duration = 1f;
+
+		// Setup
+		song_preview_player.Stop();
+		song_preview_player.clip = Track.song;
+		song_preview_player.time = Track.preview_start_time;
+		song_preview_player.volume = 0;
+		song_preview_player.Play();
+
+		// Fade in
+		while (song_preview_player.volume < volume) {
+			song_preview_player.volume += Time.deltaTime / fade_in_duration * volume;
+			if (song_preview_player.volume > volume) {
+				song_preview_player.volume = volume;
+			}
+			yield return null;
+		}
+
+		// Wait
+		yield return new WaitForSeconds(song_preview_duration);
+
+		// Fade out
+		while (song_preview_player.volume > 0) {
+			song_preview_player.volume -= Time.deltaTime / fade_out_duration * volume;
+			yield return null;
+		}
+
+		song_preview_player.Stop();
+	}
+}
