32fa7520c33b9f265c7508455435170235d0989d
Zixuan Zou <>
2019-02-21 18:30:35 -0500

    added credits button/panel on main menu

---
+++ Beatsketball/Assets/Scripts/creditsPanel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class creditsPanel : MonoBehaviour
+{
+    public GameObject credits_panel;
+    bool credits_on;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        credits_panel.SetActive(false);
+        credits_on = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void showCredits()
+    {
+        if (credits_on == false)
+        {
+            credits_panel.SetActive(true);
+            credits_on = true;
+        }
+        else
+        {
+            credits_panel.SetActive(false);
+            credits_on = false;
+        }
+
+    }
+}
